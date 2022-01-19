a68b2ef3d2fbcfcf9d93ff7c9031a48a1c8792da
joey-bertschler <joey.bertschler@gmail.com>
2022-01-19 10:39:25 +0400

    seasons

--- index.js
+++ index.js
@@ -89,6 +89,8 @@ class Seasons {
    */
   constructor() {
     // ✨ initialize whatever properties are needed
+    this.seasons = ["summer", "fall", "winter", "spring"];
+    this.currentSeason = 0
   }
 
   /**
@@ -105,6 +107,13 @@ class Seasons {
    */
   next() {
     // ✨ implement
+    const result = this.seasons[this.currentSeason];
+    if (this.currentSeason === 3) {
+      this.currentSeason = 0
+    } else {
+      this.currentSeason++;
+    } 
+    return result;
   }
 }
 
